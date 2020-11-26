d342ddfcd33683d2214dc2739d85c2b47fb11825
unknown <arafatshuvo18@gmail.com>
2020-11-26 23:04:35 +0600

    commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,78 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { createContext, useState } from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+//import AddEvent from './Component/AddEvent/Addevent';
+import AddEvent from './Component/AddEvent/AddEvent';
+import Admin from './Component/Admin/Admin';
+import Blog from './Component/Blog/Blog';
+import Donation from './Component/Donation/Donation';
+import Header from './Component/Header/Header';
+import HeroArea from './Component/HeroArea/HeroArea';
+//import Login from './Component/Login/Login';
+import Login from './Component/Login/firebaseConfig';
+//import NotFound from './Component/NotFound/NotFound';
+import NotFound from './Component/Notfound/NotFound';
+import PrivateRoute from './Component/PrivateRoute/PrivateRoute';
+import RegEvent from './Component/RegEvent/RegEvent';
+import Register from './Component/Register/Register';
+
+export const UserContext = createContext();
 
 function App() {
+  const [loggedInUser , setLoggedInUser] = useState({
+      name : '',
+      email : '',
+      date: '',
+      description:'',
+      img:''
+  });
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <UserContext.Provider value={[loggedInUser , setLoggedInUser]}>
+      <Router>
+        <Switch>
+            <Route exact path='/'>
+                <HeroArea></HeroArea>
+            </Route>
+            <Route path='/home'>
+                <HeroArea></HeroArea>
+            </Route>
+            <Route path='/login'>
+                <Login />
+            </Route>
+            <Route path='/RegEvent'>
+                <RegEvent />
+            </Route>
+            <PrivateRoute path='/register/:key'>
+                <Register></Register>
+            </PrivateRoute>
+            <Route path='/register'>
+                <Login />
+            </Route>
+            <Route path='/blog'>
+                <Blog />
+            </Route>
+            <Route path='/events'>
+                <RegEvent />
+            </Route>
+            <Route path='/donation' >
+                <Donation />
+            </Route>
+            <Route path='/admin' >
+                <Admin />
+            </Route>
+            <Route path='/addEvent' >
+                <AddEvent />
+            </Route>
+            <Route path='*' >
+                <NotFound />
+            </Route>
+        </Switch>
+      </Router>
+    </UserContext.Provider>
   );
 }
-
 export default App;
---
+++ src/Component/Login/firebaseConfig.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCWRRunyQ3BT2mdwL6XIHW5e7Gt6sLQx-E",
+    authDomain: "volunteer-32a93.firebaseapp.com",
+    databaseURL: "https://volunteer-32a93.firebaseio.com",
+    projectId: "volunteer-32a93",
+    storageBucket: "volunteer-32a93.appspot.com",
+    messagingSenderId: "878438706010",
+    appId: "1:878438706010:web:7c7955f3d97d744b3de163"
+  };
+  export default firebaseConfig;
